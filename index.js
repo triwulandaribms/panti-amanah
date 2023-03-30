de9c8d88780a246b95b6e126e4f98a9a08076685
Tri Wulandari <triwulandari3456@gamil.com>
2023-03-30 16:00:28 +0700

    four push

--- index.js
+++ index.js
@@ -11,15 +11,25 @@ import bcrypt from "bcryptjs";
 
 const app = express();
 
-app.use(express.json());
-
+// MIDDLEWARE
+// untuk mengelola cookienya
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  if (req.path.startsWith("/assets") || req.path.startsWith("/api/login")) {
+  if (req.path.startsWith("/api/login") || req.path.startsWith("/assets")) {
     next();
   } else {
+    let authorized = false;
     if (req.cookies.token) {
+      try {
+        jwt.verify(req.cookies.token, process.env.SECRET_KEY);
+        authorized = true;
+      } catch (err) {
+        res.setHeader("Cache-Control", "no-store"); // khusus untuk vercel
+        res.clearCookie("token");
+      }
+    }
+    if (authorized) {
       if (req.path.startsWith("/login")) {
         res.redirect("/");
       } else {
@@ -29,22 +39,33 @@ app.use((req, res, next) => {
       if (req.path.startsWith("/login")) {
         next();
       } else {
-        res.redirect("/login");
+        if (req.path.startsWith("/api")) {
+          res.status(401);
+          res.send("Anda harus login terlebih dahulu.");
+        } else {
+          res.redirect("/login");
+        }
       }
     }
   }
 });
 
+// BAGIAN UNTUK MENGAKSES FILE STATIS
+app.use(express.static("public"));
+
 // import path from "path";
 // const __dirname = path.dirname(new URL(import.meta.url).pathname);
 // app.use(express.static(path.resolve(__dirname, "public")));
 
-app.use(express.static("public"));
+// BAGIAN UNTUK MEMBACA BODY BERFORMAT JSON
+app.use(express.json());
 
+// ROUTE OTORISASI
 app.post("/api/login", async (req, res) => {
   const results = await client.query(
     `SELECT * FROM admin WHERE id = '${req.body.id}'`
   );
+  console.log(results);
   if (results.rows.length > 0) {
     if (await bcrypt.compare(req.body.password, results.rows[0].password)) {
       const token = jwt.sign(results.rows[0], process.env.SECRET_KEY);
@@ -60,24 +81,14 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-app.use((req, res, next) => {
-  if (req.cookies.token) {
-    try {
-      jwt.verify(req.cookies.token, process.env.SECRET_KEY);
-      next();
-    } catch (err) {
-      res.status(401);
-      res.send("Anda harus login lagi.");
-    }
-  } else {
-    res.status(401);
-    res.send("Anda harus login terlebih dahulu.");
-  }
-});
-
-app.get("/api/saya", (req, res) => {
-  const me = jwt.verify(req.cookies.token, process.env.SECRET_KEY);
-  res.json(me);
+app.post("/api/admin", async (req, res) => {
+  const salt = await bcrypt.genSalt();
+  const hash = await bcrypt.hash(req.body.password, salt);
+  console.log(hash);
+  await client.query(
+    `INSERT INTO admin (id,password,username) VALUES ('${req.body.id}', ('${req.body.username}, '${hash}')`
+  );
+  res.send("Berhasil login");
 });
 
 // ROUTE DATA ANAK
@@ -94,22 +105,21 @@ app.get("/api/anak/:nik_anak", async (req, res) => {
   res.json(results.rows[0]);
 });
 
-const salt = await bcrypt.genSalt();
-const hash = await bcrypt.hash("michalec031", salt);
-console.log(hash);
-
-app.post("/api/anak", async (req, res) => {
-  const salt = await bcrypt.genSalt();
-  const hash = await bcrypt.hash(req.body.password, salt);
-  await client.query(
-    `INSERT INTO anak (nik_anak,nama,umur,jenis_kelamin) VALUES ('${req.body.nik_anak}', '${req.body.nama}',${req.body.umur}', '${req.body.jenis_kelamin}','${hash}')`
-  );
-  res.send("anak berhasil ditambahkan.");
-});
+// app.post("/api/anak", async (req, res) => {
+//   const salt = await bcrypt.genSalt();
+//   const hash1 = await bcrypt.hash(req.body.password, salt);
+//   const hash2 = await bcrypt.hash(req.body.nik_anak, salt);
+//   await client.query(
+//     `INSERT INTO anak (nik_anak,nama,umur,jenis_kelamin) VALUES ('${req.body.nik_anak}', '${req.body.nama}',${req.body.umur}', '${req.body.jenis_kelamin}','${hash1}',${hash2})`
+//   );
+//   res.send("anak berhasil ditambahkan.");
+// });
 
 app.put("/api/anak/:nik_anak", async (req, res) => {
+  const salt = await bcrypt.genSalt();
+  const hash = await bcrypt.hash(req.body.nik_anak, salt);
   await client.query(
-    `UPDATE anak SET nik_anak = '${req.body.nik_anak}', nama = '${req.body.nama}', umur = '${req.body.umur}', jenis_kelamin = '${req.body.jenis_kelamin}' WHERE nik_anak = ${req.params.nik_anak}`
+    `UPDATE anak SET nik_anak = '${hash}', nama = '${req.body.nama}', umur = '${req.body.umur}', jenis_kelamin = '${req.body.jenis_kelamin}' WHERE nik_anak = ${req.params.nik_anak}`
   );
   res.send("anak berhasil diedit.");
 });
